316313d3e8d9b866729f65503561ec0d7f226924
hs92 <himanshu84207@gmail.com>
2020-09-01 12:46:22 +0530

    Added JUnits for Service layer.

--- qualification-test/src/test/java/com/wns/csv/qualificationtest/QualificationTestApplicationTests.java
+++ qualification-test/src/test/java/com/wns/csv/qualificationtest/QualificationTestApplicationTests.java
@@ -72,9 +72,33 @@ class QualificationTestApplicationTests {
 
 		List<VehcatDto> ciList = vehcatData.stream().filter(dto -> "CI".equals(dto.getVehcat())).collect(Collectors.toList());
 		List<VehcatDto> guList = vehcatData.stream().filter(dto -> "GU".equals(dto.getVehcat())).collect(Collectors.toList());
+		List<VehcatDto> bkList = vehcatData.stream().filter(dto -> "BK".equals(dto.getVehcat())).collect(Collectors.toList());
 
+		long gioCount = critEntity.stream().filter(dto -> dto.getGioRule() != null && dto.getGioacpt() != null).count();
+		long ampCount = critEntity.stream().filter(dto -> dto.getAmpacpt() != null && dto.getAmprule() != null).count();
+		long aamCount = critEntity.stream().filter(dto -> dto.getAamacpt() != null && dto.getAamrule() != null).count();
+		long apiaCount = critEntity.stream().filter(dto -> dto.getApiacpt() != null && dto.getApiRule() != null).count();
+		long suncorpCount = critEntity.stream().filter(dto -> dto.getSunRule() != null && dto.getSunacpt() != null).count();
+		long shannonsCount = critEntity.stream().filter(dto -> dto.getShnRule() != null && dto.getShnacpt() != null).count();
+		long imrCount = critEntity.stream().filter(dto -> dto.getImraCpt() != null && dto.getImrRule() != null).count();
+
+		long expectedSize = gioCount + ampCount + aamCount + apiaCount + suncorpCount + shannonsCount + imrCount;
+
+		// checking if no other vehicle cat is present in result
 		assertEquals(0, ciList.size());
 		assertEquals(0, guList.size());
+
+		// checked for count of companies
+		assertEquals(gioCount, bkList.stream().filter(b -> "GIO".equals(b.getCompany())).count());
+		assertEquals(ampCount, bkList.stream().filter(b -> "AMP".equals(b.getCompany())).count());
+		assertEquals(aamCount, bkList.stream().filter(b -> "AAMI".equals(b.getCompany())).count());
+		assertEquals(apiaCount, bkList.stream().filter(b -> "APIA".equals(b.getCompany())).count());
+		assertEquals(suncorpCount, bkList.stream().filter(b -> "SUNCORP".equals(b.getCompany())).count());
+		assertEquals(shannonsCount, bkList.stream().filter(b -> "SHANNONS".equals(b.getCompany())).count());
+		assertEquals(imrCount, bkList.stream().filter(b -> "IMR".equals(b.getCompany())).count());
+
+		// total size
+		assertEquals(expectedSize, vehcatData.size());
 	}
 
 	@Test
@@ -92,9 +116,12 @@ class QualificationTestApplicationTests {
 		List<VehcatDto> ciList = vehcatData.stream().filter(dto -> ("CI".equals(dto.getVehcat()) && dto.getNvic().startsWith("!"))).collect(Collectors.toList());
 		List<VehcatDto> bkList = vehcatData.stream().filter(dto -> "BK".equals(dto.getVehcat())).collect(Collectors.toList());
 
+		long giociCount = critEntity.stream().filter(dto -> dto.getGiociRule() != null && dto.getGioacpt() != null && !dto.getNvic().startsWith("!")).count();
+
 		assertEquals(0, guList.size());
 		assertEquals(0, ciList.size());
 		assertEquals(0, bkList.size());
+		assertEquals(giociCount, vehcatData.stream().filter(b -> "GIOCI".equals(b.getCompany())).count());
 	}
 
 	@Test
@@ -109,9 +136,37 @@ class QualificationTestApplicationTests {
 
 		List<VehcatDto> ciList = vehcatData.stream().filter(dto -> "CI".equals(dto.getVehcat())).collect(Collectors.toList());
 		List<VehcatDto> bkList = vehcatData.stream().filter(dto -> "BK".equals(dto.getVehcat())).collect(Collectors.toList());
+		List<VehcatDto> guList = vehcatData.stream().filter(dto -> "GU".equals(dto.getVehcat())).collect(Collectors.toList());
+
+		long aamCount = critEntity.stream().filter(dto -> dto.getAamacpt() != null && dto.getAamrule() != null).count();
+		long apiaCount = critEntity.stream().filter(dto -> dto.getApiacpt() != null && dto.getApiRule() != null).count();
+		long suncorpCount = critEntity.stream().filter(dto -> dto.getSunRule() != null && dto.getSunacpt() != null).count();
+		long vo3Count = critEntity.stream().filter(dto -> dto.getVo3rule() != null && dto.getVo3acpt() != null).count();
+		long vo5Count = critEntity.stream().filter(dto -> dto.getVo5acpt() != null && dto.getV05rule() != null).count();
+		long gioCount = critEntity.stream().filter(dto -> dto.getGioRule() != null && dto.getGioacpt() != null).count();
+		long essentialsCount = critEntity.stream().filter(dto -> dto.getEssRule() != null && dto.getEssacpt() != null).count();
+		long bingleCount = critEntity.stream().filter(dto -> dto.getBingleRule() != null && dto.getBingleacpt() != null).count();
+		long giociCount = critEntity.stream().filter(dto -> dto.getGiociRule() != null && dto.getGioacpt() != null).count();
+		long jciCount = critEntity.stream().filter(dto -> dto.getJciRule() != null && dto.getJciacpt() != null).count();
+		long shannonsCount = critEntity.stream().filter(dto -> dto.getShnRule() != null && dto.getShnacpt() != null).count();
+		long ampCount = critEntity.stream().filter(dto -> dto.getAmpacpt() != null && dto.getAmprule() != null).count();
 
 		assertEquals(0, ciList.size());
 		assertEquals(0, bkList.size());
+
+		// checking for individual companies
+		assertEquals(gioCount, guList.stream().filter(b -> "GIO".equals(b.getCompany())).count());
+		assertEquals(ampCount, guList.stream().filter(b -> "AMP".equals(b.getCompany())).count());
+		assertEquals(aamCount, guList.stream().filter(b -> "AAMI".equals(b.getCompany())).count());
+		assertEquals(apiaCount, guList.stream().filter(b -> "APIA".equals(b.getCompany())).count());
+		assertEquals(suncorpCount, guList.stream().filter(b -> "SUNCORP".equals(b.getCompany())).count());
+		assertEquals(shannonsCount, guList.stream().filter(b -> "SHANNONS".equals(b.getCompany())).count());
+		assertEquals(vo3Count, guList.stream().filter(b -> "VERO3".equals(b.getCompany())).count());
+		assertEquals(vo5Count, guList.stream().filter(b -> "VERO5".equals(b.getCompany())).count());
+		assertEquals(essentialsCount, guList.stream().filter(b -> "ESSENTIALS".equals(b.getCompany())).count());
+		assertEquals(bingleCount, guList.stream().filter(b -> "BINGLE".equals(b.getCompany())).count());
+		assertEquals(jciCount, guList.stream().filter(b -> "JCI".equals(b.getCompany())).count());
+		assertEquals(giociCount, guList.stream().filter(b -> "GIOCI".equals(b.getCompany())).count());
 	}
 
 	@Test
